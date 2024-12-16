46828bd78df7374cfbc6ce7bc211ee0bec822c8c
nehuenmorales <nehuenmorales640@gmail.com>
2024-12-16 02:02:14 -0300

    change name interface

--- src/app/Services/images-pixabay.service.ts
+++ src/app/Services/images-pixabay.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {  DataImage } from '../Interfaces/data-image';
+import {  DataImage } from '../Interfaces/dataImage';
 import { catchError, map, Observable, throwError } from 'rxjs';
 import { ApiResponse } from '../Interfaces/apiResponse';
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./Components/nav/nav.component";
 import { CommonModule } from '@angular/common';
 import { PhotoComponent } from "./Components/photo/photo.component";
-import {  DataImage } from "./Interfaces/data-image"
+import {  DataImage } from "./Interfaces/dataImage"
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
